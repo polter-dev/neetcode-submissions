7d341d9ffb97e9ec1f2c373d983209a8108db7bf
polter-dev <144177408+polter-dev@users.noreply.github.com>
2026-05-22 21:59:55 -0400

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+    {
+        if (!list1) return list2;
+        if (!list2) return list1;
+        if (!list1 && !list2) return NULL;
+        
+        ListNode *ret;
+        ListNode *walker1 = list1, *walker2 = list2;
+
+        if (walker1->val < walker2->val)
+        {
+            ret = walker1;
+            walker1 = walker1->next;
+        }
+        else
+        {
+            ret = walker2;
+            walker2 = walker2->next;
+        }
+
+        ListNode *newHead = ret;
+        while(walker1 && walker2) 
+        {
+            if (walker2->val < walker1->val)
+            {
+                ret->next = walker2;
+                walker2 = walker2->next;
+                ret = ret->next;
+            }
+            else
+            {
+                ret->next = walker1;
+                ret = ret->next;
+                walker1 = walker1->next;
+            }
+
+        }
+
+        if (walker1)
+            ret->next = walker1;
+        else if (walker2)
+            ret->next = walker2;
+
+        return newHead;
+    }
+};
